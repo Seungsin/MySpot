5973d33b486ecf2744fe1a74abd4a97b9041efe9
seungsin <sinny@sinnys-MacBookPro.local>
2022-07-10 23:08:04 +0900

    [Start]make gradle file

---
+++ myspot/src/test/java/com/myspot/myspot/MyspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspot.myspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
